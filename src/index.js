1772f37fb858f2f629aad6fd3589b98aab676ecb
Xtr126 <80520774+Xtr126@users.noreply.github.com>
2024-07-13 21:56:35 +0530

    Use full layout

--- src/index.js
+++ src/index.js
@@ -3,8 +3,63 @@ import Keyboard from "simple-keyboard";
 import "simple-keyboard/build/css/index.css";
 import "./index.css";
 
-let keyboard = new Keyboard({
-  onKeyPress: button => onKeyPress(button)
+let commonKeyboardOptions = {
+  onKeyPress: button => onKeyPress(button),
+  theme: "simple-keyboard hg-theme-default hg-layout-default",
+  syncInstanceInputs: true,
+  mergeDisplay: true
+};
+
+let keyboard = new Keyboard(".simple-keyboard-main", {
+  onKeyPress: button => onKeyPress(button),
+  layout: {
+    'default': [
+      "esc f1 f2 f3 f4 f5 f6 f7 f8 f9 f10 f11 f12",
+      "` 1 2 3 4 5 6 7 8 9 0 - = {backspace}",
+      "{tab} Q W E R T Y U I O P { } |",
+      '{capslock} A S D F G H J K L : " {enter}',
+      "{shiftleft} Z X C V B N M < > ? {shiftright}",
+      "{controlleft} fn win {altleft} {space} {altright} {controlleft}"
+    ],
+  },
+  display: {
+    "{tab}": "tab <>",
+    "{backspace}": "<- backspace",
+    "{enter}": "<- enter",
+    "{capslock}": "caps lock ^",
+    "{shiftleft}": "shift ^",
+    "{shiftright}": "shift ^",
+    "{controlleft}": "ctrl",
+    "{controlright}": "ctrl",
+    "{altleft}": "alt",
+    "{altright}": "alt",
+    "{meta}": "win",
+    "{space}": "space"
+  }
+});
+
+let keyboardControlPad = new Keyboard(".simple-keyboard-control", {
+  ...commonKeyboardOptions,
+  layout: {
+    default: [
+      "{prtscr} {scrolllock} {pause}",
+      "{insert} {home} {pageup}",
+      "{delete} {end} {pagedown}"
+    ]
+  }
+});
+
+let keyboardArrows = new Keyboard(".simple-keyboard-arrows", {
+  ...commonKeyboardOptions,
+  layout: {
+    default: ["{arrowup}", "{arrowleft} {arrowdown} {arrowright}"]
+  },
+  display: {
+    "{arrowup}": "^",
+    "{arrowleft}": "<",
+    "{arrowdown}": "v",
+    "{arrowright}": ">"
+  }
 });
 
 console.log(keyboard);
@@ -16,6 +71,5 @@ socket.binaryType = 'arraybuffer';
 
 function onKeyPress(button) {
   console.log("Button pressed", button);
-  if (socket.OPEN)
-    socket.send(button);
+  socket.send(button);
 }
